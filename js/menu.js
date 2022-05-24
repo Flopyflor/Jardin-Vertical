a7ee9edb79c86979770c7b64d9cbcb956cde57c2
Florencia Allami <flopyflor20@gmail.com>
2022-05-24 16:24:20 -0300

    Cambios estéticos. Otra vez falta arreglar los links para git-hosting

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,17 @@
 // funcion para activar el menu
 function menu() {
     document.getElementById("nav").style.display = "flex";
+    document.getElementById("nav").style.top = "0px";
+    setTimeout(() => {
+        document.getElementById("nav").style.height = "100vh";
+    }, 1)
 }
 
 // funcion para sacar el menu
 function exitMenu() {
-    document.getElementById("nav").style.display = "none";
+    document.getElementById("nav").style.height = "0vh";
+    document.getElementById("nav").style.top = "-10px";
+    setTimeout(() => {
+        document.getElementById("nav").style.display = "none";
+    }, 490)
 }
